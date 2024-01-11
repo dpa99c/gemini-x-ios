e8bf1a02bf048ca1af60aae0ba4d562c424a9c78
Dave Alden <dave@workingedge.co.uk>
2024-01-11 12:32:57 +0000

    Initial implementation

--- GeminiX.swift
+++ GeminiX.swift
@@ -1,58 +1,159 @@
 import GoogleGenerativeAI
 
-class GeminiX {
-    
-    static var model: GenerativeModel?
-    
-    
-    static func initModel(params:[String: Any]) {
-        
-        let modelName = params["modelName"] as? String ?? ""
-        let apiKey = params["apiKey"] as? String ?? ""
-        
-        let temperature = params["temperature"] != nil ? params["temperature"] as? Float : nil
-        let topK = params["topK"] != nil ? params["topK"] as? Int : nil
-        let topP = params["topP"] != nil ? params["topP"] as? Float : nil
-        let maxOutputTokens = params["maxOutputTokens"] != nil ? params["maxOutputTokens"] as? Int : nil
-        let stopSequences = params["stopSequences"] != nil ? params["stopSequences"] as? [String] : nil
-        
-        let safetySettings = params["safetySettings"] != nil ? params["safetySettings"] as? [String: String] : nil
+protocol HistoryPart {
+    var type: String { get }
+    var content: Any { get }
+}
 
-        
-        // TODO safetySettings
-        
-        let generationConfig = GenerationConfig(
-            temperature: temperature,
-            topP: topP,
-            topK: topK,
-            maxOutputTokens: maxOutputTokens,
-            stopSequences: stopSequences
-        )
-        
-        self.model = GenerativeModel(name:modelName, apiKey:apiKey, generationConfig:generationConfig)
+class TextHistoryPart: HistoryPart {
+    var content: Any
+
+    var type: String {
+        return "text"
     }
-    
-    static func sendMessage(inputText:String, onSuccess:@escaping(String) -> Void, onError:@escaping(String) -> Void) {
+
+    init(content: String) {
+        self.content = content
+    }
+}
+
+class ImageHistoryPart: HistoryPart {
+    var content: Any
+
+    var type: String {
+        return "image"
+    }
+
+    init(content: UIImage) {
+        self.content = content
+    }
+}
+
+class BlobHistoryPart: HistoryPart {
+    var content: Any
+    var mimeType: String
+
+    var type: String {
+        return "blob"
+    }
+
+    init(content: Data, mimeType: String) {
+        self.content = content
+        self.mimeType = mimeType
+    }
+}
+
+class HistoryItem {
+    var parts: [HistoryPart]
+    var isUser: Bool
+
+    init(parts: [HistoryPart], isUser: Bool) {
+        self.parts = parts
+        self.isUser = isUser
+    }
+}
+
+enum GeminiXError: Error {
+    case invalidArgument(String)
+}
+
+class GeminiX {
+
+
+    /****************
+     * Internal properties
+     ***********************/
+
+    private static var model: GenerativeModel?
+    private static var chat: Chat?
+
+    /******************
+     * Public functions
+     **************************/
+
+    static func initModel(onSuccess:() -> Void, onError:(String) -> Void, params:[String: Any]) {
+        do{
+
+            let modelName = params["modelName"] as? String ?? ""
+            let apiKey = params["apiKey"] as? String ?? ""
+
+            let temperature = params["temperature"] != nil ? params["temperature"] as? Float : nil
+            let topK = params["topK"] != nil ? params["topK"] as? Int : nil
+            let topP = params["topP"] != nil ? params["topP"] as? Float : nil
+            let maxOutputTokens = params["maxOutputTokens"] != nil ? params["maxOutputTokens"] as? Int : nil
+            let stopSequences = params["stopSequences"] != nil ? params["stopSequences"] as? [String] : nil
+
+            let str_safetySettings = params["safetySettings"] != nil ? params["safetySettings"] as? [String: String] : nil
+            var safetySettings = [] as [SafetySetting]
+            for (key, value) in str_safetySettings! {
+                safetySettings.append(SafetySetting(harmCategory: try getHarmCategory(category: key), threshold: try getHarmLevel(level: value)))
+            }
+
+
+            let generationConfig = GenerationConfig(
+                temperature: temperature,
+                topP: topP,
+                topK: topK,
+                maxOutputTokens: maxOutputTokens,
+                stopSequences: stopSequences
+            )
+
+            self.model = GenerativeModel(name:modelName, apiKey:apiKey, generationConfig:generationConfig, safetySettings: safetySettings)
+            self.chat = nil
+            onSuccess()
+        }catch{
+            onError("\(error)")
+        }
+    }
+
+    static func sendMessage(onSuccess:@escaping(String, Bool, _ callback:@escaping() -> Void) -> Void, onError:@escaping(String) -> Void, inputText:String, images:[UIImage]?, streamResponse:Bool?) {
+        if((self.model == nil)){
+            onError("Model not initialised")
+            return
+        }
         Task.detached(operation: {
-            // TODO support images for multi-modal models
-            
             do{
-                let response = try await self.model?.generateContent(inputText)
-                let result = (response?.text)!
-                onSuccess(result)
+                let inputContents = getInputContents(text:inputText, images:images ?? [], onError:onError)
+               
+                if(streamResponse == true){
+                    guard let contentStream = model?.generateContentStream(inputContents) else {
+                        onError("No content stream in response")
+                        return;
+                    }
+                    let group = DispatchGroup()
+                    for try await chunk in contentStream {
+                        if let text = chunk.text {
+                            group.enter()
+                            onSuccess(text, false, {
+                                group.leave()
+                            }) 
+                        }
+                    }
+                    group.notify(queue: .main) {
+                        onSuccess("", true, {})
+                    }
+                }else{
+                    let response = try await self.model?.generateContent(inputContents)
+                    let result = (response?.text)!
+                    onSuccess(result, true, {})
+                }
             }catch{
                 onError("\(error)")
             }
         })
-        
+
     }
 
-    static func countTokens(inputText:String, onSuccess:@escaping(String) -> Void, onError:@escaping(String) -> Void) {
+    static func countTokens(onSuccess:@escaping(String) -> Void, onError:@escaping(String) -> Void, inputText:String, images:[UIImage]?) {
+        if((self.model == nil)){
+            onError("Model not initialised")
+            return
+        }
         Task.detached(operation:{
-            // TODO support images for multi-modal models
-            
             do{
-                let response = try await self.model?.countTokens(inputText)
+                let inputContents = getInputContents(text:inputText, images:images ?? [], onError:onError)
+                
+                let response = try await self.model?.countTokens(inputContents)
                 let result = (response?.totalTokens)!
                 onSuccess("\(result)")
             }catch{
@@ -61,4 +162,195 @@ class GeminiX {
         })
     }
 
+    static func initChat(onSuccess:() -> Void, onError:(String) -> Void, history:[HistoryItem]?) {
+        if((self.model == nil)){
+            onError("Model not initialised")
+            return
+        }
+
+        var chatHistory:[ModelContent] = []
+        if(history != nil){
+            for item in history! {
+                let userRole = item.isUser ? "user" : "model"
+                for part in item.parts {
+                    if(part.type == "text"){
+                        chatHistory.append(ModelContent(role: userRole, parts: part.content as! String))
+                    }else if(part.type == "image"){
+                        chatHistory.append(ModelContent(role: userRole, parts: part.content as! UIImage))
+                    }else if(part.type == "blob"){
+                        let part = part as! BlobHistoryPart
+                        let blob = part.content as! Data
+                        let mimeType = part.mimeType
+                        let dataPart = ModelContent.Part.data(mimetype: mimeType, blob)
+                        chatHistory.append(ModelContent(role: userRole, parts: dataPart))
+                    }
+                }
+            }
+        }
+
+        self.chat = self.model?.startChat(history: chatHistory)
+        onSuccess()
+    }
+    
+    static func sendChatMessage(onSuccess:@escaping(String, Bool, _ callback:@escaping() -> Void) -> Void, onError:@escaping(String) -> Void, inputText:String, images:[UIImage]?, streamResponse:Bool?) {
+        if((self.model == nil)){
+            onError("Model not initialised")
+            return
+        }
+        if((self.chat == nil)){
+            onError("Chat not initialised")
+            return
+        }
+        Task.detached(operation: {
+            do{
+                let inputContents = getInputContents(text:inputText, images:images ?? [], onError:onError)
+                
+                if(streamResponse == true){
+                    guard let contentStream = self.chat?.sendMessageStream(inputContents) else {
+                        onError("No content stream in response")
+                        return;
+                    }
+                    let group = DispatchGroup()
+                    for try await chunk in contentStream {
+                        if let text = chunk.text {
+                            group.enter()
+                            onSuccess(text, false, {
+                                group.leave()
+                            })
+                        }
+                    }
+                    group.notify(queue: .main) {
+                        onSuccess("", true, {})
+                    }
+                }else{
+                    let response = try await self.chat?.sendMessage(inputContents)
+                    let result = (response?.text)!
+                    onSuccess(result, true, {})
+                }
+            }catch{
+                onError("\(error)")
+            }
+        })
+
+    }
+
+    static func countChatTokens(onSuccess:@escaping(String) -> Void, onError:@escaping(String) -> Void, inputText:String?, images:[UIImage]?) {
+        if((self.model == nil)){
+            onError("Model not initialised")
+            return
+        }
+        if((self.chat == nil)){
+            onError("Chat not initialised")
+            return
+        }
+        Task.detached(operation:{
+            do{
+                var inputContents:[ModelContent] = self.chat?.history ?? []
+                if(inputText != nil && images != nil && images!.count > 0){
+                    let userInputContents = getInputContents(text:inputText ?? "", images:images ?? [], onError:onError)
+                    inputContents.append(contentsOf:userInputContents)
+                }
+                
+                let response = try await self.model?.countTokens(inputContents)
+                let result = (response?.totalTokens)!
+                onSuccess("\(result)")
+            }catch{
+                onError("\(error)")
+            }
+        })
+    }
+
+    static func getChatHistory(onSuccess:@escaping([HistoryItem]) -> Void, onError:@escaping(String) -> Void) {
+            if((self.model == nil)){
+                onError("Model not initialised")
+                return
+            }
+            if((self.chat == nil)){
+                onError("Chat not initialised")
+                return
+            }
+            
+            var chatHistory:[ModelContent] = self.chat?.history ?? []
+            var history:[HistoryItem] = []
+            for chatContent in chatHistory{
+                var isUser = chatContent.role == "user"
+                var historyParts:[HistoryPart] = []
+                for chatPart in chatContent.parts {
+                    switch chatPart {
+                      case let .text(textVal):
+                        let historyPart = TextHistoryPart(content: chatPart.text!)
+                        historyParts.append(historyPart)
+                      case let .data(mimetypeVal, dataVal):
+                        let historyPart = BlobHistoryPart(content: dataVal, mimeType: mimetypeVal)
+                        historyParts.append(historyPart)
+                      }
+                }
+                let historyItem = HistoryItem(parts:historyParts, isUser:isUser)
+                history.append(historyItem)
+            }
+            onSuccess(history)
+        }
+
+    /******************
+     * Internal functions
+     **************************/
+    
+    private static func getHarmCategory(category:String) throws -> SafetySetting.HarmCategory{
+        var harmCategory = SafetySetting.HarmCategory.unknown
+        switch category {
+        case "DANGEROUS_CONTENT":
+            harmCategory = SafetySetting.HarmCategory.dangerousContent
+        case "HARASSMENT":
+            harmCategory = SafetySetting.HarmCategory.harassment
+        case "HATE_SPEECH":
+            harmCategory = SafetySetting.HarmCategory.hateSpeech
+        case "SEXUALLY_EXPLICIT":
+            harmCategory = SafetySetting.HarmCategory.sexuallyExplicit
+        case "UNSPECIFIED":
+            harmCategory = SafetySetting.HarmCategory.unspecified
+        default:
+            throw GeminiXError.invalidArgument("\(category) is not a valid Harm Category")
+        }
+        return harmCategory
+    }
+    
+    private static func getHarmLevel(level:String) throws -> SafetySetting.BlockThreshold{
+        var threshold = SafetySetting.BlockThreshold.unknown
+        switch level {
+        case "NONE":
+            threshold = SafetySetting.BlockThreshold.blockNone
+        case "ONLY_HIGH":
+            threshold = SafetySetting.BlockThreshold.blockOnlyHigh
+        case "MEDIUM_AND_ABOVE":
+            threshold = SafetySetting.BlockThreshold.blockMediumAndAbove
+        case "LOW_AND_ABOVE":
+            threshold = SafetySetting.BlockThreshold.blockLowAndAbove
+        case "UNSPECIFIED":
+            threshold = SafetySetting.BlockThreshold.unspecified
+        default:
+            throw GeminiXError.invalidArgument("\(level) is not a valid Harm Level")
+        }
+        return threshold
+    }
+    
+
+    private static func getInputContents(text:String, images:[UIImage], onError:@escaping(String) -> Void) -> [ModelContent] {
+        var parts:[ModelContent.Part] = []
+        if(text != ""){
+            let textPart = ModelContent.Part.text(text)
+            parts.append(textPart)
+        }
+    
+        for image in images{
+            if let data = image.jpegData(compressionQuality: 0.8) {
+                let imagePart = ModelContent.Part.data(mimetype: "image/jpeg", data)
+                parts.append(imagePart)
+            }else{
+                onError("Unable to create JPG from UIImage data")
+            }
+        }
+        let inputContent = ModelContent(role: "user", parts: parts)
+        let inputContents = [inputContent]
+        return inputContents
+    }
 }
